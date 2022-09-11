a8687d42a62a11fda8c5a5d59cddf9791480cf48
Sasha <112004995+Sasha5151@users.noreply.github.com>
2022-09-11 20:16:43 +0300

    37

--- Program.cs
+++ Program.cs
@@ -89,38 +89,73 @@
  // Задача 35. Массив из 123 случ чисел
  // найдите колличество чисел в интервале от 10 до 99
 
-int size = 123;
+// int size = 123;
+// int[] array = new int[size];
+// FullArray(array);
+// PrintArray(array);
+
+// void FullArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0,1000);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//        System.Console.Write(array[i] + " "); 
+//     }
+//     System.Console.WriteLine();
+// }
+
+// int FindeNum(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if ((array[i] > 10 ) && (array[i] <99 ))
+//         count++;
+//     }
+//     return count;
+// } 
+// FindeNum(array);
+// System.Console.WriteLine();
+// System.Console.WriteLine(FindeNum(array));
+
+//End
+
+// Задача 37. одномерный массив попарно умножить 
+
+
+int size = 8;
 int[] array = new int[size];
 FullArray(array);
 PrintArray(array);
-
+System.Console.WriteLine();
+Mult(array);
 void FullArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0,1000);
+        array[i] = new Random().Next(0,10);
     }
 }
-
 void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
        System.Console.Write(array[i] + " "); 
     }
-    System.Console.WriteLine();
 }
-
-int FindeNum(int[] array)
+void Mult(int[] array)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    int[] array1 = new int [array.Length/2];
+    for (int i = 0 , j = array.Length-1 ; i < array.Length/2; i++ , j--)
     {
-        if ((array[i] > 10 ) && (array[i] <99 ))
-        count++;
-    }
-    return count;
-} 
-FindeNum(array);
-System.Console.WriteLine();
-System.Console.WriteLine(FindeNum(array));
+        array1[i] = array[i]*array[j];
+        System.Console.WriteLine(array1[i]);
+    }    
+}
